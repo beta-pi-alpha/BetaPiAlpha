d7dacfad791fcdae6cc8fd4ff59211e557ef3341
Beta-Pi-Alpha <41700825+beta-pi-alpha@users.noreply.github.com>
2018-07-28 01:32:20 +0200

    Create dll_main.cpp

---
+++ Injection/dll_main.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+#include <Windows.h>
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+		MessageBoxA(NULL,
+			"Injection Done",
+			"BetaPiAlpha",
+			MB_OK);
+		break;
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
